e5cf418ed19b644c55eb627d9e985842ac6be8a4
Jax Choong <choongjax@gmail.com>
2024-09-08 11:43:16 +0800

    feat: now has error checking

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,36 @@
 const usersStorage = require("../storage/usersStorage");
+const {body,validationResult} = require("express-validator");
+
+const alphaErr = "Must only contain letters!";
+const lengthErr = "Must be between 1 and 10 characters!";
+
+
+// validators
+const validateUser = [
+  body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+  body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+];
+
+// We can pass an entire array of middleware validations to our controller.
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  }
+];
 
 exports.usersListGet = (req, res) => {
   res.render("index", {
@@ -13,8 +45,3 @@ exports.usersCreateGet = (req, res) => {
   });
 };
 
-exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
-  res.redirect("/");
-};
